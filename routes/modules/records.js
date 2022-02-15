46f77c51dfc49f009f8fdc6089e9202b92fab63e
Young <phanton0117@gmail.com>
2022-02-15 23:08:48 +0800

    feat: add edit handlebars page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -14,11 +14,13 @@ router.post('/', (req, res) => {
 
 router
   .route('/:id')
-  .get((req, res) => {
-
-  })
   .put((req, res) => {
+    req.body.userId = userId = req.user._id
+    _id = req.params.id
 
+    recordModel.findOneAndUpdate({ _id, userId }, req.body)
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
   })
   .delete((req, res) => {
     const userId = req.user._id
@@ -29,8 +31,26 @@ router
       .catch(error => console.log(error))
   })
 
-router.get('/:id/edit', (req, res) => {
-  
+router.get('/:id/edit', async (req, res) => {
+   const userId = req.user._id
+
+  const _id = req.params.id
+
+  let categoryArray = await categoryModel.find({}).lean().then()
+
+  // find a currentRecord with recordID(_id) and userId
+  recordModel.findOne({ _id, userId })
+    .populate('categoryId', 'name')
+    .lean()
+    .then((currentRecord) => {
+      // transfer date to YYYY-MM-DD format 
+      // YYYY -> Year
+      // MM -> Month
+      // DD -> Day
+      currentRecord.date = moment(currentRecord.date).format('YYYY-MM-DD')
+      res.render('edit', { categoryArray, currentRecord })
+    })
+
 })
 
 module.exports = router
